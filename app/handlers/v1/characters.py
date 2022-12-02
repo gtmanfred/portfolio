05d5fe26a9574c2484c84467cf4cf27cba796e33
Daniel Wallace <danielwallace@gtmanfred.com>
2022-12-02 11:02:46 -0500

    use vue instead of jinja templating

--- app/handlers/v1/characters.py
+++ app/handlers/v1/characters.py
@@ -12,26 +12,15 @@ class Response(Character):
     seed: uuid.UUID
 
 
-@router.get('/bastards', response_model=Response, operation_id="get_new_character")
-def get_new_character(commoner: bool = False, extra_classes: bool = False):
-    seed = uuid.uuid4()
+@router.get('/bastards', response_model=Response, operation_id="get_new_character", tags=['bastards'])
+@router.get('/bastards/{character_id}', response_model=Response, operation_id="get_character", tags=['bastards'])
+def get_character(commoner: bool = False, extra_classes: bool = False, character_id: uuid.UUID = None):
+    seed = uuid.uuid4() if character_id is None else character_id
     return {
         "seed": seed,
         **CharacterGenerator.create(
             commoner=commoner,
             extra_classes=extra_classes,
             seed=seed,
-        ).dict(),
-    }
-
-
-@router.get('/bastards/{character_id}', response_model=Response, operation_id="get_character")
-def get_character(character_id: uuid.UUID, commoner: bool = False, extra_classes: bool = False):
-    return {
-        "seed": character_id,
-        **CharacterGenerator.create(
-            commoner=commoner,
-            extra_classes=extra_classes,
-            seed=character_id,
         ).dict(),
     }
