c533ff7efefc35a65c4689a11b94b01b7712232e
Daniel Wallace <danielwallace@gtmanfred.com>
2022-12-03 00:04:02 -0500

    clean up data and use template files

--- app/bastards.py
+++ app/bastards.py
@@ -1,6 +1,3 @@
-import uuid
-
-from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 
 def _register_bastards(app):
     app.mount('/static', StaticFiles(directory='app/static', html=True))
+    app.mount('/templates', StaticFiles(directory='app/templates', html=True))
     app.get('/bastards/', response_class=HTMLResponse)(bastards_page)
     app.get('/bastards/{character_id}', response_class=HTMLResponse)(bastards_page)
 
---
+++ app/static/sheet.js
@@ -0,0 +1,65 @@
+const Sheet = fetch('/templates/bastards.html').then((response) => response.text()).then((body) => {
+  return {
+    template: body,
+    computed: {
+      character_id() {
+        return this.$route.params.character_id;
+      },
+    },
+    created() {
+      new SwaggerClient('/openapi.json').then(
+        (client) => {
+          if (this.$route.params.character_id) {
+            client.apis.bastards.get_character({character_id: this.$route.params.character_id}).then((resp) => {
+              this.character = resp.body;
+            });
+          } else {
+            client.apis.bastards.get_new_character().then((resp) => {
+              this.character = resp.body;
+            });
+          };
+        },
+      );
+    },
+    methods: {
+      getLoad() {
+        let current_load = 0;
+        for (i=0; i<this.character.inventory.length; i++) {
+          current_load += this.character.inventory[i].slots;
+        };
+        return `${current_load}/${this.character.max_load}`;
+      },
+      getWeaponData(item){
+        let note = item.size;
+        if (item.throwable) {
+          note += ", throwable";
+        };
+        if (item.twohanded) {
+          note += ", two-handed";
+        };
+        if (item.ranged) {
+          note += ", ranged";
+        };
+        return `${note}`;
+      },
+      copyLink() {
+        navigator.clipboard.writeText(`${window.location.origin}/bastards/${this.character.seed}${window.location.search}`);
+      },
+      createCharacter() {
+        router.go({ path: '/bastards/'})
+      },
+      createCommoner() {
+        router.go({ name: 'sheet', query: {commoner: true}})
+      },
+      createWithExtraClasses() {
+        router.go({ name: 'sheet', query: {extra_classes: true}})
+      },
+    },
+    data() {
+      return {
+        character: null,
+      };
+    },
+  };
+});
+
