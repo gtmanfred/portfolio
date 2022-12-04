6a72e6ca7d51b01fa128cc3cd07a00462796a5e4
Daniel Wallace <danielwallace@gtmanfred.com>
2022-12-03 21:03:14 -0500

    drop extra directory

--- app/bastards/__init__.py
+++ app/bastards/__init__.py
@@ -4,5 +4,4 @@ from ..utils.spa import SinglePageApp
 
 
 def _register_bastards(app):
-    app.mount('/bastards/static', app=StaticFiles(directory='app/bastards/static'))
-    app.mount('/bastards/', app=SinglePageApp(directory='app/bastards/html', html=True))
+    app.mount('/bastards/', app=SinglePageApp(directory='app/bastards/static', html=True))
